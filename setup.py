c7639e39dadd119ef4fd1c34e2980804f8f8a025
Nikhil8684 <nikhilv8484@gmail.com>
2024-06-07 23:07:47 +0530

    change 2

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'fishDetection',
+    version= '0.0.0',
+    author= 'Nikhil V',
+    author_email= 'nikhilv8484@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
